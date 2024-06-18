ddfe07104314c6ab1e27844cb5e9572cbfe9e338
alpinerum <35735448+alpinerum@users.noreply.github.com>
2024-06-18 11:49:41 -0400

    created routing files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Cmp1Component } from './cmp1/cmp1.component';
+import { Cmp2Component } from './cmp2/cmp2.component';
+import { Cmp3Component } from './cmp3/cmp3.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'cmp1', component: Cmp1Component},
+  {path: 'cmp2', component: Cmp2Component},
+  {path: 'cmp3', component: Cmp3Component},
+  {path:'**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Cmp1Component } from './cmp1/cmp1.component';
+import { Cmp2Component } from './cmp2/cmp2.component';
+import { Cmp3Component } from './cmp3/cmp3.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    Cmp1Component,
+    Cmp2Component,
+    Cmp3Component,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
